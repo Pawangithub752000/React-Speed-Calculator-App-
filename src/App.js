2e36e45537c83821d82d86fd9b2a88fd292f8129
Pawan Kumar <pawanbtechcse752000@gmail.com>
2023-01-25 15:31:52 +0530

    All most Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
+
+function App(){
+  const [distance, setDistance] = useState();
+  const [time, setTime] = useState();
+  const [speed, setSpeed] = useState();
+
+  function handleDistance(e){
+    // console.log(e.target.value)
+    setDistance(e.target.value);
+  }
+  function handleTime(e){
+    setTime(e.target.value);
+  }
+  function calculateSpeed(){
+  setSpeed(distance/time);
+  }
+  function clearSpeed(){
+  setSpeed();
+  }
+
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <h1>Speed Calculator</h1>
 
-export default App;
+
+      <div className="input_div">
+        <input type="number" placeholder="Distance" value={distance} onChange={handleDistance}/>
+        <input type="number" placeholder="Time" value={time} onChange={handleTime}/>
+      </div>
+
+      <div className='button_div'>
+        <button onClick={calculateSpeed}>Calculate Speed</button>
+        <button onClick={clearSpeed}>Clear Speed</button>
+      </div>
+
+      <div>{speed}
+      
+      
+      
+      </div>
+
+
+    </div>
+  )
+
+}
+export default App
