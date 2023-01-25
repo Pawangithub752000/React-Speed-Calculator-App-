d07b3b8d70b8da3dbf1114d90e9d7380dcdb75af
Pawan Kumar <pawanbtechcse752000@gmail.com>
2023-01-25 21:29:56 +0530

    All functionality done

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
 
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 
-function App(){
+function App() {
   const [distance, setDistance] = useState();
   const [time, setTime] = useState();
   const [speed, setSpeed] = useState();
 
-  function handleDistance(e){
+  function handleDistance(e) {
     // console.log(e.target.value)
     setDistance(e.target.value);
   }
-  function handleTime(e){
+  function handleTime(e) {
     setTime(e.target.value);
   }
-  function calculateSpeed(){
-  setSpeed(distance/time);
+  function calculateSpeed() {
+    setSpeed(distance / time);
   }
-  function clearSpeed(){
-  setSpeed();
+  function clearSpeed() {
+    setSpeed();
   }
 
 
-  return(
+  return (
     <div className="App">
       <h1>Speed Calculator</h1>
 
 
       <div className="input_div">
-        <input type="number" placeholder="Distance" value={distance} onChange={handleDistance}/>
-        <input type="number" placeholder="Time" value={time} onChange={handleTime}/>
+       <span><input type="number" placeholder="Distance" value={distance} onChange={handleDistance} /> </span> 
+        <input type="number" placeholder="Time" value={time} onChange={handleTime} />
       </div>
 
       <div className='button_div'>
-        <button onClick={calculateSpeed}>Calculate Speed</button>
-        <button onClick={clearSpeed}>Clear Speed</button>
+        <span> <button onClick={calculateSpeed}>Calculate Speed</button> </span>
+        <span><button onClick={clearSpeed}>Clear Speed</button></span>
       </div>
 
-      <div>{speed}
-      
-      
-      
-      </div>
+      <div>
+        <h1>{speed <= 60 ? `Slow ${speed} km/hr.`
+          : speed > 60 && speed < 100 ? `Fast ${speed} km/hr.`
+          : speed >= 100 ? `Very Fast {speed} km/hr.`
+          : null}</h1>
+     </div>
 
 
     </div>
